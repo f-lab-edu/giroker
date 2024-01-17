a63a3819f9b987eaf6d4d43811e338fb283b8e17
tolluset <dlqud19@gmail.com>
2024-01-18 02:59:46 +0900

    chore: add get activites mock

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,15 @@
 import { HttpResponse, http } from "msw";
 
-const getActivityList = http.get("getActivityList", () => {
-  const result = [{ id: "1" }];
+const getActivityList = http.get("/activities", () => {
+  const result = [
+    {
+      id: "1",
+      title: "사이드 프로젝트/giroker",
+      description: "시간단위로 기록하는 프로젝트",
+    },
+  ];
 
-  return HttpResponse.json(result);
+  return HttpResponse.json({ data: result });
 });
 
-const handlers = [getActivityList];
-
-export { handlers };
+export const handlers = [getActivityList];
