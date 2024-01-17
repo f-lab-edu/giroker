ed2d5b33fef0b77ee7aa8b07bd5726e97491733c
tolluset <dlqud19@gmail.com>
2024-01-18 03:00:20 +0900

    chore: add api env constants

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,6 @@
 const IS_API_MOCKING = process.env.NEXT_PUBLIC_API_MOCKING === "enable";
+const API_ENDPOINT = IS_API_MOCKING
+  ? "http://localhost:9090"
+  : process.env.NEXT_PUBLIC_API_ENDPOINT;
 
-export { IS_API_MOCKING };
+export { IS_API_MOCKING, API_ENDPOINT };
