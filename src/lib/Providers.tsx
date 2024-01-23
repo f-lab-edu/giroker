f43c16106b690007b9974288a89198b2b1451451
tolluset <dlqud19@gmail.com>
2024-01-23 20:24:17 +0900

    chore: add setter

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -3,8 +3,12 @@
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(
+export default function Providers({
+  children,
+}: {
+  children: Readonly<React.ReactNode>;
+}) {
+  const [queryClient, _] = useState(
     () =>
       new QueryClient({
         defaultOptions: {
