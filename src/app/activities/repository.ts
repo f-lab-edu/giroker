bc4ea4b429a8ef5dfab59f46139fd1e7352d802c
tolluset <dlqud19@gmail.com>
2024-03-01 01:02:52 +0900

    refactor: use template literal sql query

--- src/app/activities/repository.ts
+++ src/app/activities/repository.ts
@@ -41,23 +41,19 @@ export const repository: ActivityRepository = {
   async findAll({ order, date }) {
     const session = await auth();
 
-    const dateToYYYY_MM_DD = (date: Date) =>
-      date.toISOString().substring(0, 10);
+    // converted to YYYY-MM-DD
+    const YYYYMMDD = (date: Date) => date.toISOString().substring(0, 10);
 
-    const today = date ? dateToYYYY_MM_DD(date) : undefined;
+    const today = date ? YYYYMMDD(date) : YYYYMMDD(new Date());
 
-    let query = `SELECT * FROM activities WHERE "userId" = ${session.user.id}`;
-
-    if (date) {
-      // KR +9 tz
-      query += `
-            AND created_at >= DATE '${today} 15:00:00' 
-            AND created_at < DATE '${today} 15:00:00' + INTERVAL '1 day'`;
-    }
-
-    query += ` ORDER BY Id ${order}`;
-
-    const result = await sql.query(query);
+    // fragment is not supported yet, so use sql.query
+    // https://github.com/vercel/storage/issues/495
+    const result = await sql.query(
+      `SELECT * FROM activities WHERE "userId" = ${session.user.id} 
+        AND created_at >= '${today} 15:00:00'
+        AND created_at < DATE '${today} 15:00:00' + INTERVAL '1 day'
+        ORDER BY Id ${order}`,
+    );
     return result.rows as unknown as Activity[];
   },
 
@@ -65,36 +61,36 @@ export const repository: ActivityRepository = {
     const session = await auth();
     const now = Date.now() / 1000.0;
 
-    await sql.query(`INSERT INTO activities ("userId", name, description, created_at)
-                     VALUES ('${session.user.id}', '${activity.name} ', '${activity.description}', to_timestamp(${now}))`);
+    await sql`INSERT INTO activities ("userId", name, description, created_at)
+                     VALUES ('${session.user.id}', '${activity.name} ', '${activity.description}', to_timestamp(${now}))`;
   },
 
   async update({ activity }: { activity: Activity }) {
     const session = await auth();
 
-    await sql.query(`UPDATE activities
-                     SET memo = '${activity.memo}' 
+    await sql`UPDATE activities
+                     SET memo = ${activity.memo} 
                      WHERE "userId" = ${session.user.id}
-                            AND id = ${activity.id}`);
+                            AND id = ${activity.id}`;
   },
 
   async start({ activityId, startedAt }) {
     const session = await auth();
 
-    await sql.query(`UPDATE activities
+    await sql`UPDATE activities
                      SET status = 'playing',
                          started_at = to_timestamp(${startedAt})
                      WHERE "userId" = ${session.user.id}
-                           AND id = ${activityId}`);
+                           AND id = ${activityId}`;
   },
 
   async stop({ activityId, stoppedAt }) {
     const session = await auth();
 
-    await sql.query(`UPDATE activities
+    await sql`UPDATE activities
                      SET status = 'stopped',
                          stopped_at = to_timestamp(${stoppedAt})
                      WHERE "userId" = ${session.user.id}
-                           AND id = ${activityId}`);
+                           AND id = ${activityId}`;
   },
 };
